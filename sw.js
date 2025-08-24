bd371fef8fd80ecf319a8e7dcda4e1ce54c12b7c
harutarooo <harutarooo@users.noreply.github.com>
2025-08-24 03:04:23 +0000

    deploy: f6d6999fa2728446a3146782603fdce334616305

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'routine-cache-v1';
-const CORE_ASSETS = [ '/', '/index.html', '/manifest.webmanifest' ];
+const CORE_ASSETS = [ './', './index.html', './manifest.webmanifest', './icons/icon-daily-routine.png' ];
 self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE_ASSETS))); });
 self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); });
 self.addEventListener('fetch', e=>{
